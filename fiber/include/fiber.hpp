9ce9243001921784033423abf316049eccbc3004
mathcai <gmathcai@gmail.com>
2016-01-01 00:17:38 +0800

    add a basic ucontext base fiber

--- fiber/include/fiber.hpp
+++ fiber/include/fiber.hpp
@@ -6,13 +6,25 @@
 #include <functional>
 #include <thread>
 #include <iostream>
+#include <ostream>
+#include <string>
+#include <stdexcept>
 
 #include <cstdint>
 #include <cassert>
 
+#ifdef USE_BOOST_COROTUINE
 #include <boost/coroutine/coroutine.hpp>
+#else
+#include <ucontext.h>
+#endif
 
 namespace fiber {
+
+class fiber_error: public std::runtime_error {
+public:
+    explicit fiber_error(const std::string& what): std::runtime_error(what) { }
+};
 
 enum class fiber_status {
     running,
@@ -21,135 +33,247 @@ enum class fiber_status {
     dead,
 };
 
-std::ostream& operator<<(std::ostream &o, fiber_status status) {
+template<class CharT, class Traits>
+std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& out, fiber_status status) {
     switch (status) {
     case fiber_status::running:
-        o << "running";
-        break;
+        return out << "running";
     case fiber_status::suspended:
-        o << "suspended";
-        break;
+        return out << "suspended";
     case fiber_status::normal:
-        o << "normal";
-        break;
+        return out << "normal";
     case fiber_status::dead:
-        o << "dead";
-        break;
+        return out << "dead";
     }
-    return o;
+    return out;
 }
 
-class __fiber_block_base;
+/*
+class __impl_base;
 
 namespace this_fiber { 
 
-__fiber_block_base* __this_fiber_block() noexcept;
+__impl_base* __this_fiber_impl() noexcept;
 
 void yield();
 
-}
+}*/
 
-class __fiber_block_base {
+class __fiber_base {
+protected:
+    class __impl_base;
+
+    class __basic_impl;
+
+    template<class Fn>
+    class __fiber_impl;
+
+public:
+    class __this_fiber_helper;
+};
+
+class __fiber_base::__impl_base {
+    friend class __fiber_base::__this_fiber_helper;
+
+protected:
+    __impl_base *__parent_impl;
+    fiber_status __status;
+
+    struct __unwind {
+        __unwind(__impl_base& impl) noexcept: __impl(impl) { __set_thread_impl(&__impl); }
+        ~__unwind() noexcept {
+            __set_thread_impl(__impl.__parent_impl);
+            __impl.__set_status(fiber_status::dead);
+        }
+        __impl_base& __impl;
+    };
+
+    __impl_base() noexcept: __parent_impl(__thread_impl()), __status(fiber_status::running) {
+        if (__parent_impl) { __parent_impl->__set_status(fiber_status::normal); }
+    }
+
+    __impl_base(const __impl_base&) = delete;
+
+    virtual ~__impl_base() {};
+
+    __impl_base& operator=(const __impl_base&) = delete;
+
+    virtual void __fiber_routine() = 0;
+
+    inline fiber_status __get_status() const noexcept { return __status; }
+
+    inline void __set_status(fiber_status status) noexcept { __status = status; }
+
+    void __set_resume() { 
+        assert(__status == fiber_status::suspended);
+        __set_thread_impl(this); 
+        __set_status(fiber_status::running); 
+    }
+
+    void __set_yield() { 
+        assert(__thread_impl() == this && __status == fiber_status::running);
+        __set_thread_impl(__parent_impl); 
+        __set_status(fiber_status::suspended); 
+    }
+
+private:
+    static inline void __set_thread_impl(__impl_base *impl) noexcept { __thread_impl() = impl; }
+
+    static __impl_base*& __thread_impl() noexcept {
+        static __thread __impl_base* _thread_impl;
+        return _thread_impl;
+    }
+};
+
+
+class __fiber_base::__basic_impl: public __fiber_base::__impl_base { 
     friend class fiber;
-    friend __fiber_block_base* this_fiber::__this_fiber_block() noexcept;
-    friend void this_fiber::yield();
+    friend class __fiber_base::__this_fiber_helper;
 
+protected:
+    typedef const void* __native_handle_type;
+
+    inline __native_handle_type __native_handle() const noexcept { return __native_handle_type(this); }
+
+#ifdef USE_BOOST_COROTUINE
+private:
     typedef boost::coroutines::coroutine<void> coroutine_type;
     typedef coroutine_type::push_type yield_type;
     typedef coroutine_type::pull_type fiber_type;
-    typedef const void *native_handle_type;
 
-    __fiber_block_base *__parent_block;
-    fiber_status __status;
     yield_type *__fiber_yield;
     fiber_type __fiber;
 
-    struct __unwind {
-        __unwind(__fiber_block_base& block) noexcept: __block(block) {
-            __set_thread_block(&__block);
-        }
-        ~__unwind() noexcept {
-            __block.__on_unwind();
-        }
-        __fiber_block_base& __block;
-    };
-
-    static inline void __set_thread_block(__fiber_block_base *block) noexcept {
-        __thread_block() = block;
-    }
-
-    static inline __fiber_block_base*& __thread_block() noexcept {
-        static __thread __fiber_block_base* _thread_block;
-        return _thread_block;
-    }
-
-    void __set_status(fiber_status status) noexcept { __status = status; }
-
-    virtual void _fiber_block_entry() = 0;
-
-    __fiber_block_base():
-        __parent_block(__thread_block()),
-        __status(fiber_status::running),
-        __fiber_yield(nullptr),
-        __fiber(std::bind(&__fiber_block_base::__routine, this, std::placeholders::_1)) {}
-
-    __fiber_block_base(const __fiber_block_base&) = delete;
-
-    __fiber_block_base& operator=(const __fiber_block_base&) = delete;
-
     void __routine(yield_type &yield) {
+        __impl_base::__unwind _unwind(*this);
+
         __fiber_yield = &yield;
-        __unwind _unwind(*this);
 
         //yield from here for complete construct of fiber
         __yield();
 
-        _fiber_block_entry();
+        __fiber_routine();
     }
 
-    void __on_unwind() noexcept {
-        __set_thread_block(__parent_block);
-        __set_status(fiber_status::dead);
+protected:
+    __basic_impl(): __impl_base(), __fiber_yield(nullptr), __fiber(std::bind(&__basic_impl::__routine, this, std::placeholders::_1)) { }
+
+    void __resume() { __set_resume(); __fiber(); }
+
+    void __yield() { assert(__fiber_yield); __set_yield(); (*__fiber_yield)(); }
+
+#else
+private:
+    ucontext_t __context; 
+    ucontext_t &__parent_context;
+    char __stack[102400];
+
+    static void __ucontext_entry(unsigned int hthis, unsigned int lthis) {
+        __basic_impl *that = reinterpret_cast<__basic_impl *>((static_cast<unsigned long>(hthis) << 32) | lthis); 
+
+        __impl_base::__unwind _unwind(*that);
+
+        //yield from here for complete construct of fiber
+        that->__yield();
+
+        that->__fiber_routine();
+    }
+
+    void __routine() {
+        if (getcontext(&__context) != 0) {
+            throw fiber_error("getcontext error"); 
+        }
+        
+        __context.uc_stack.ss_sp = __stack;
+        __context.uc_stack.ss_size = sizeof(__stack);
+        __context.uc_link = &__parent_context;
+
+        unsigned int hthis = static_cast<unsigned int>(reinterpret_cast<unsigned long>(this) >> 32);
+        unsigned int lthis = static_cast<unsigned int>(reinterpret_cast<unsigned long>(this) & 0xffffffff);
+        makecontext(&__context, reinterpret_cast<void (*)()>(__ucontext_entry), 2, hthis, lthis);
+
+        if (swapcontext(&__parent_context, &__context) != 0) {
+            throw fiber_error("swapcontext error");
+        }
+    }
+
+    ucontext_t &__get_parent_context() {
+        static __thread ucontext_t _thread_context;
+        return __parent_impl ? static_cast<__basic_impl *>(__parent_impl)->__context : _thread_context;
+    }
+
+protected:
+    __basic_impl(): __impl_base(), __parent_context(__get_parent_context()) { __routine(); }
+
+    void __yield() {
+        __set_yield();
+        if (swapcontext(&__context, &__parent_context) != 0) {
+            throw fiber_error("swapcontext error");
+        }
     }
 
     void __resume() {
-        assert(__status == fiber_status::suspended);
-        __set_thread_block(this);
-        __set_status(fiber_status::running);
-        __fiber();
+        __set_resume();
+        if (swapcontext(&__parent_context, &__context) != 0) {
+            throw fiber_error("swapcontext error");
+        }
     }
 
-    void __yield() {
-        assert(__fiber_yield && __thread_block() == this && __status == fiber_status::running);
-        __set_thread_block(__parent_block);
-        __set_status(fiber_status::suspended);
-        (*__fiber_yield)();
-    }
-
-    inline fiber_status __get_status() const noexcept { return __status; }
-
-public:
-    virtual ~__fiber_block_base() {};
-
-    inline native_handle_type native_handle() const noexcept { return this; }
+#endif
 };
 
-class fiber {
-    //typedef std::allocator<char> allocator_type;
+
+template <class Fn>
+class __fiber_base::__fiber_impl: public __fiber_base::__basic_impl {
+    Fn __fn;
+
 public:
-    typedef __fiber_block_base::native_handle_type id;
+    __fiber_impl(Fn&& fn): __fiber_base::__basic_impl(), __fn(std::forward<Fn>(fn)) {
+        //now, construct done and resume this fiber
+        __resume();
+    }
 
-private:
-    template <class Fn>
-    class __fiber_block: public __fiber_block_base {
-        Fn __fn;
+    void __fiber_routine() override { __fn(); }
+};
+
+class __fiber_base::__this_fiber_helper {
+    static __fiber_base::__basic_impl* __this_fiber_impl() {
+        return reinterpret_cast<__fiber_base::__basic_impl *>(__fiber_base::__impl_base::__thread_impl());
+    }
+public:
+    static void __yield() { 
+        __fiber_base::__basic_impl* impl = __this_fiber_impl();
+        assert(impl);
+        impl->__yield();
+    };
+    static __fiber_base::__basic_impl::__native_handle_type __native_handle() noexcept {
+        __fiber_base::__basic_impl* impl = __this_fiber_impl(); 
+        assert(impl);
+        return impl->__native_handle();
+    }
+    static bool __in_fiber() noexcept {
+        return static_cast<bool>(__fiber_base::__impl_base::__thread_impl());
+    }
+};
+
+class fiber: public __fiber_base {
+public:
+    typedef typename __fiber_base::__basic_impl::__native_handle_type native_handle_type;
+
+    class id {
+        friend class fiber;
+
+        native_handle_type __fiber;
+
     public:
-        __fiber_block(Fn&& fn): __fiber_block_base(), __fn(std::forward<Fn>(fn)) {}
+        id() noexcept : __fiber() {}
 
-        void _fiber_block_entry() override {
-            //try {
-            __fn();
-        }
+        explicit id(native_handle_type f) noexcept : __fiber(f) {} 
+
+        native_handle_type native_handle() const noexcept { return __fiber; }
+
+        template<class CharT, class Traits>
+        friend std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& out, fiber::id id);
     };
 
 public:
@@ -160,9 +284,7 @@ public:
     fiber(fiber&& f) noexcept { swap(f); }
 
     template <class Fn, class... Args>
-    explicit fiber(Fn&& fn, Args&&... args): __block(__make_shared_block( std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...) )) { 
-        //now, construct done and resume this fiber
-        resume();
+    explicit fiber(Fn&& fn, Args&&... args): __impl(__make_shared_impl( std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...) )) { 
     }
 
     ~fiber() noexcept { } 
@@ -171,51 +293,46 @@ public:
 
     fiber& operator=(fiber&& f) noexcept { swap(f); return *this; }
 
-    void swap(fiber &f) noexcept { __block.swap(f.__block); } 
+    void swap(fiber &f) noexcept { __impl.swap(f.__impl); } 
 
-    void resume() { __block->__resume(); }
+    void resume() { __impl->__resume(); }
 
-    void suspend() { }
+    fiber_status get_status() const noexcept { return __impl->__get_status(); }
 
-    fiber_status get_status() const noexcept { return __block->__get_status(); }
-
-    id get_id() const noexcept { return id(__block->native_handle()); }
+    id get_id() const noexcept { return id(__impl->__native_handle()); }
 
     static unsigned int hardware_concurrency() noexcept {
         return std::thread::hardware_concurrency();
     }
 
 private:
-    std::shared_ptr<__fiber_block_base> __block;
+    std::shared_ptr<__fiber_base::__basic_impl> __impl;
 
     template <class Fn>
-    std::shared_ptr<__fiber_block<Fn>> __make_shared_block(Fn&& f) {
-        return std::make_shared<__fiber_block<Fn>>(std::forward<Fn>(f));
+    static std::shared_ptr<__fiber_base::__fiber_impl<Fn>> __make_shared_impl(Fn&& f) {
+        return std::make_shared<__fiber_base::__fiber_impl<Fn>>(std::forward<Fn>(f));
     }
-
 };
+
+
+template<class CharT, class Traits>
+std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& out, fiber::id id) {
+    return out << id.native_handle();
+}
 
 
 namespace this_fiber {
 
-inline __fiber_block_base* __this_fiber_block() noexcept {
-    return __fiber_block_base::__thread_block();
-}
-
 void yield() {
-    __fiber_block_base* block = __this_fiber_block();
-    assert(block);
-    block->__yield();
+    __fiber_base::__this_fiber_helper::__yield();
 }
 
 fiber::id get_id() noexcept {
-    __fiber_block_base* block = __this_fiber_block();
-    assert(block);
-    return fiber::id(block->native_handle());
+    return fiber::id(__fiber_base::__this_fiber_helper::__native_handle());
 }
 
-bool is_fiber() noexcept {
-    return static_cast<bool>(__this_fiber_block());
+bool in_fiber() noexcept {
+    return __fiber_base::__this_fiber_helper::__in_fiber();
 }
 
 } //this_fiber
